303ed428802e972fa1b76e6684006e776b6b4a61
csblock <153804872+csblock@users.noreply.github.com>
2023-12-15 11:07:37 +0600

    Update shopify-script.js

--- shopify-script.js
+++ shopify-script.js
@@ -1,14 +1,1 @@
-// Create a style element
-var style = document.createElement('style');
 
-// Add CSS to the style element
-style.innerHTML = `
-
- div#shopify-section-template--16320972718257__heading{
-     //opacity:.5!important;
- }
- 
-`;
-
-// Append the style element to the head of the document
-document.head.appendChild(style);
